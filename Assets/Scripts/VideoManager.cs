bc83337e208c2e8b021c26b70bf6b9f8296509f5
ShunIshii <ishiishun10@gmail.com>
2020-07-28 17:55:18 +0900

    Select Video

---
+++ Assets/Scripts/VideoManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEditor;
+
+public class VideoManager : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickStartPauseButton()
+    {
+        if (!videoPlayer.isPlaying)
+        {
+            videoPlayer.Play();
+        }
+        else
+        {
+            videoPlayer.Pause();
+        }
+    }
+
+    public void OnClickSelectButton()
+    {
+        videoPlayer.source = VideoSource.Url;
+        videoPlayer.url = EditorUtility.OpenFilePanel("Select video", "", "mov, mp4");
+    }
+}
