f0aa3652b31f93c581344983368789597dc22b8b
ShunIshii <ishiishun10@gmail.com>
2020-07-29 18:46:01 +0900

    Add current bar

---
+++ Assets/Scripts/CurrentBarBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CurrentBarBehaviour : MonoBehaviour
+{
+    private float startPosX;
+    [SerializeField] private GameObject graphPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetStartPos()
+    {
+        var graphWidth = graphPanel.GetComponent<RectTransform>().sizeDelta.x;
+        startPosX = -(graphWidth / 2);
+        transform.position = transform.position + new Vector3(startPosX, 0, 0);
+    }
+
+    public void OnClickSetStartButton()
+    {
+        SetStartPos();
+    }
+}
