6701e886fbcb4c4db241982756af1926b4fd477c
ShunIshii <ishiishun10@gmail.com>
2020-09-27 18:15:39 +0900

    Move plots

---
+++ Assets/Scripts/PlotsBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlotsBehaviour : MonoBehaviour, IDragHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        transform.position += new Vector3(data.delta.x, 0f, 0f);
+    }
+}
