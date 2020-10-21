e75a8c4240a80c67cfe3c4d519af4f3da348107a
ShunIshii <ishiishun10@gmail.com>
2020-10-21 19:24:47 +0900

    Layout changed

--- Assets/Scripts/GraphGenerator.cs
+++ Assets/Scripts/GraphGenerator.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System;
 using UnityEditor;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public struct Point
 {
@@ -39,20 +40,28 @@ public class GraphGenerator : MonoBehaviour
 
     private void plot(List<Point> points)
     {
-        float graphWidth = GetComponent<RectTransform>().sizeDelta.x;
-        float graphHeight = GetComponent<RectTransform>().sizeDelta.y;
-        double fs = points.Count / points.Last().elaspedTime;
-        float interval = graphWidth / points.Count;
-        float startPos = -(graphWidth / 2);
-        Debug.Log("Fs = " + (int)fs + "Hz");
+        //float graphWidth = GetComponent<RectTransform>().sizeDelta.x;
+        //float graphHeight = GetComponent<RectTransform>().sizeDelta.y;
+        //double fs = points.Count / points.Last().elaspedTime;
+        float interval = transform.lossyScale.y;// graphWidth / points.Count;
+        //float startPos = -(graphWidth / 2);
+        //Debug.Log("Fs = " + (int)fs + "Hz");
 
         pointsList = new List<GameObject>();
 
         float height = transform.lossyScale.y * 5;
+        float graphHeight = transform.GetComponent<RectTransform>().offsetMin.y - transform.GetComponent<RectTransform>().offsetMax.y;
+        Debug.Log(graphHeight);
 
         foreach (var p in points.Select((value, index) => new { value, index}))
         {
-            GameObject newPoint = Instantiate(pointPrefab, transform.position + new Vector3(startPos + interval * p.index, p.value.y * height, 0), Quaternion.identity, plotsObj.transform);
+            GameObject newPoint = Instantiate(pointPrefab);
+            newPoint.transform.SetParent(plotsObj.transform);
+            newPoint.transform.localScale = new Vector3(1, 1, 1);
+            newPoint.GetComponent<RectTransform>().anchorMin = new Vector2(0, 0.5f);
+            newPoint.GetComponent<RectTransform>().anchorMax = new Vector2(0, 0.5f);
+            newPoint.GetComponent<RectTransform>().pivot = new Vector2(0.5f, 0.5f);
+            newPoint.transform.position = new Vector3(interval * p.index, p.value.y * height + graphHeight * 6, 0);
             pointsList.Add(newPoint);
             if (p.index != 0)
             {
@@ -63,14 +72,22 @@ public class GraphGenerator : MonoBehaviour
                 Vector3 newPosition = pointA.transform.position + dtPos / 2;
                 double newRotation = Math.Atan2(dtPos.y, dtPos.x) * 180d / Math.PI;
 
-                GameObject line = Instantiate(linePrefab, newPosition, Quaternion.Euler(0, 0, (float)newRotation), plotsObj.transform);
-                
+                GameObject line = Instantiate(linePrefab, newPosition, Quaternion.Euler(0, 0, (float)newRotation));
+
                 // Chage line length
                 RectTransform t = line.GetComponent<RectTransform>();
                 double lineLength = Math.Sqrt(Math.Pow(dtPos.x, 2) + Math.Pow(dtPos.y, 2));
                 t.sizeDelta = new Vector2((float)lineLength, t.sizeDelta.y);
+
+                line.transform.SetParent(pointB.transform);
+                line.transform.localScale = new Vector3(1, 1, 1);
             }
         }
+        
+        Vector3 firstPointPos = pointsList[0].transform.position;
+        Vector3 lastPointPos = pointsList.Last().transform.position;
+        plotsObj.GetComponent<RectTransform>().offsetMin = new Vector2(firstPointPos.x, plotsObj.GetComponent<RectTransform>().offsetMin.y);
+        plotsObj.GetComponent<RectTransform>().offsetMax = new Vector2(lastPointPos.x, plotsObj.GetComponent<RectTransform>().offsetMax.y);
     }
 
     private void readData(string filePath)
